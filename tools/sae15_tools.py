11ed94047d49826d854b74c9edfeffc15861cf46
IliesChibane <ilies.chibane.2000@gmail.com>
2023-12-03 19:49:47 +0100

    update scrapping

--- tools/sae15_tools.py
+++ tools/sae15_tools.py
@@ -5,11 +5,27 @@ import pandas as pd
 import geopandas as gpd
 
 def loadVelibInformation() :
-  json_data = json.loads(urllib.request.urlopen('https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_information.json').read()) 
+  hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+  site = 'https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json'
+  req = urllib.request.Request(site, headers=hdr)
+  json_data = json.loads(urllib.request.urlopen(req).read()) 
   return(json_data)
 
 def loadVelibStatus() :
-  json_data = json.loads(urllib.request.urlopen('https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_status.json').read())
+  hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+  site = 'https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json'
+  req = urllib.request.Request(site, headers=hdr)
+  json_data = json.loads(urllib.request.urlopen(req).read())
   return(json_data)
 
 def getVelibStations(json_data) :
