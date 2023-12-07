86428158f030a206a35dd177c1c7c5183888dded
IliesChibane <ilies.chibane.2000@gmail.com>
2023-12-07 18:13:19 +0100

    add comments

--- tools/sae15_spec.py
+++ tools/sae15_spec.py
@@ -19,55 +19,68 @@ import contextily as ctx        # pour l'utilisation de cartes géographiques
 #------------------------------------------------------------------------------------------------
 # fonction qui retourne le taux de disponibilité des stands (en %)
 def availableDocksRate(stations_df) :
+    # Calcul du taux de disponibilité pour chaque station
+    rate = (stations_df.groupby(['stationCode'])['numDocksAvailable'].sum() / 
+            (stations_df.groupby(['stationCode'])['numBikesAvailable'].sum() + 
+             stations_df.groupby(['stationCode'])['numDocksAvailable'].sum())).fillna(0) * 100
+    
+    # Conversion du résultat en dictionnaire
+    rate = rate.to_dict()
 
-  rate = (stations_df.groupby(['stationCode'])['numDocksAvailable'].sum() / (stations_df.groupby(['stationCode'])['numBikesAvailable'].sum() + stations_df.groupby(['stationCode'])['numDocksAvailable'].sum())).fillna(0) * 100
-  rate = rate.to_dict()
-
-  return rate
+    # Retourne le dictionnaire de taux de disponibilité des stands
+    return rate
 
 #------------------------------------------------------------------------------------------------
 # fonction qui retourne le taux de disponibilité des vélos (en %)
-def availableBikesRate(stations_df) :
+def availableBikesRate(stations_df):
+    # Calcul du taux de disponibilité de vélos pour chaque station
+    rate = (stations_df.groupby(['stationCode'])['numBikesAvailable'].sum() / 
+            (stations_df.groupby(['stationCode'])['numBikesAvailable'].sum() + 
+             stations_df.groupby(['stationCode'])['numDocksAvailable'].sum())).fillna(0) * 100
+    
+    # Conversion du résultat en dictionnaire
+    rate = rate.to_dict()
 
-  rate = (stations_df.groupby(['stationCode'])['numBikesAvailable'].sum() / (stations_df.groupby(['stationCode'])['numBikesAvailable'].sum() + stations_df.groupby(['stationCode'])['numDocksAvailable'].sum())).fillna(0) * 100
-  rate = rate.to_dict()
-
-  return rate
+    # Retourne le dictionnaire de taux de disponibilité des vélos
+    return rate
 
 #------------------------------------------------------------------------------------------------
 # fonction qui retourne la date la plus récente de la mise à jour des données dynamiques
 def getLatestDate(stations_df) :
+    # Extraction et formatage de la date la plus récente des rapports des stations
+    date = stations_df['last_reported'].apply(lambda x: dt.datetime.fromtimestamp(x).strftime('%d-%m-%Y %H:%M:%S'))
 
-  date = stations_df['last_reported'].apply(lambda x: dt.datetime.fromtimestamp(x).strftime('%d-%m-%Y %H:%M:%S'))
-
-  return date
+    # Retourne la série de dates formatées
+    return date
 
 #------------------------------------------------------------------------------------------------
 # fonction qui retourne les mesures statistiques  d'une clé du DataFrame de stations
-def stationStatistics(stations_df_key) :
+def stationStatistics(stations_df_key):
+    # Initialisation d'un dictionnaire pour stocker les mesures statistiques
+    stats = dict()
 
-  stats = dict()
+    # Calcul des mesures statistiques
+    stats['mean'] = stations_df_key.mean()
+    stats['count'] = stations_df_key.count()
+    stats['std'] = stations_df_key.std()
+    stats['min'] = stations_df_key.min()
+    stats['max'] = stations_df_key.max()
 
-  stats['mean'] = stations_df_key.mean()
-  stats['count'] = stations_df_key.count()
-  stats['std'] = stations_df_key.std()
-  stats['min'] = stations_df_key.min()
-  stats['max'] = stations_df_key.max()
-
-  return stats
+    # Retourne le dictionnaire de mesures statistiques
+    return stats
 
 #------------------------------------------------------------------------------------------------
 # fonction qui exporte au format HTML le DataFrame des mesures statistiques
-def exportStatistics(stats_df, filename) :
+def exportStatistics(stats_df, filename):
+    # Convertit le DataFrame en une représentation HTML
+    html = stats_df.to_html()
 
-  html = stats_df.to_html() 
-    
-  # write html to file 
-  text_file = open(filename, "w") 
-  text_file.write(html) 
-  text_file.close() 
+    # Écrit le HTML dans un fichier
+    with open(filename, "w") as text_file:
+        text_file.write(html)
 
-  return
+    # Aucune valeur n'est retournée, le fichier est écrit directement sur le disque.
+    return
 
 #------------------------------------------------------------------------------------------------
 # fonction qui affiche et exporte la carte des stations Vélibs géolocalisées
--- tools/sae15_tools.py
+++ tools/sae15_tools.py
@@ -1,31 +1,47 @@
 import json
 import urllib
-import requests
-import pandas as pd
 import geopandas as gpd
 
-def loadVelibInformation() :
-  hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-       'Accept-Encoding': 'none',
-       'Accept-Language': 'en-US,en;q=0.8',
-       'Connection': 'keep-alive'}
-  site = 'https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json'
-  req = urllib.request.Request(site, headers=hdr)
-  json_data = json.loads(urllib.request.urlopen(req).read()) 
-  return(json_data)
+def loadVelibInformation():
+    # En-têtes HTTP pour la requête
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+           'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+           'Accept-Encoding': 'none',
+           'Accept-Language': 'en-US,en;q=0.8',
+           'Connection': 'keep-alive'}
+    
+    # URL de l'API Velib
+    site = 'https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json'
+    
+    # Crée une requête avec les en-têtes définis
+    req = urllib.request.Request(site, headers=hdr)
+    
+    # Envoie la requête et charge les données JSON
+    json_data = json.loads(urllib.request.urlopen(req).read())
+    
+    # Retourne les données au format JSON
+    return json_data
 
 def loadVelibStatus() :
+  # En-têtes HTTP pour la requête
   hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
        'Accept-Encoding': 'none',
        'Accept-Language': 'en-US,en;q=0.8',
        'Connection': 'keep-alive'}
+  
+  # URL de l'API Velib
   site = 'https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json'
+
+  # Crée une requête avec les en-têtes définis
   req = urllib.request.Request(site, headers=hdr)
+
+  # Envoie la requête et charge les données JSON
   json_data = json.loads(urllib.request.urlopen(req).read())
+
+  # Retourne les données au format JSON
   return(json_data)
 
 def getVelibStations(json_data) :
